a82d01a1b5de7253b44b5822f80710eaf7bacd95
darryl chandra <darrylchan05@gmail.com>
2025-05-15 22:40:44 +0700

    modified env

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.post('/logout', controller.postLogout);
 app.post("/mission/progress", controller.postUserMissionProgress);
 app.post('/google-signin', controller.postGoogleSignIn);
 // Use PORT from env or default to 3000
-const PORT = 3001;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
